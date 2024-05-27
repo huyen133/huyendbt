e6689bbe73d11832dc0372f6f455a53e42141ac9
huyen133 <106360070+huyen133@users.noreply.github.com>
2024-05-27 15:35:03 +0700

    Update data-extension-customer-1.sql

--- clone from wondaris/dbt-enrichment-sample/models/data-extension-customer-1.sql
+++ clone from wondaris/dbt-enrichment-sample/models/data-extension-customer-1.sql
@@ -9,7 +9,7 @@
 
 {{ config(materialized='table') }}
 
-select wondaris_customer_id, customers__attribute_gender, customers__attribute_accountcode from wndrs-mdp-internal-qa.Wondaris_Test_CDP.wndrs__customers_1662565456 limit 100
+select wondaris_customer_id, customers__attribute_gender, customers__attribute_accountcode, customers__address_country, customers__attribute_loyaltyprogram, customers__address_email, customers__address_phonenumber from wndrs-proprod-demo-clean-data.Wondaris_Test_CDP.wndrs__customers_1715098748 limit 10
 
 /*
     Uncomment the line below to remove records with null `id` values
